45f191794d31c19343cc0e53ee9d741679177bed
Hyunseok Byun <gumyoincirno@gmail.com>
2024-03-26 23:52:44 +0900

    Refectoring videojs component

--- components/vjs-component.tsx
+++ components/vjs-component.tsx
@@ -1,43 +1,85 @@
 'use client'
-import { FC, useEffect, useRef } from 'react'
+import VideoJsOptions from '@/util/videojs'
+import { FC, MutableRefObject, useEffect, useRef, useState } from 'react'
 import videojs from 'video.js'
 import Player from 'video.js/dist/types/player'
 import 'video.js/dist/video-js.css'
+import { Badge } from './ui/badge'
+import { Button } from './ui/button'
+import { useFullscreen } from './video-component/useFullscreen'
+import { usePlay } from './video-component/usePlay'
 
 type Props = {
     // 영상의 URL, 혹은 URL배열
     sources: string | { src: string; type: string }[]
     // 영상 옵션
-    options?: {
-        // 자동 재생 여부
-        autoplay?: boolean
-        // 영상 제어 여부
-        controls?: boolean
-    }
+    options?: VideoJsOptions
     //플레이어 준비 완료 시 호출 될 콜백
     onReady?: (player?: Player) => void
 }
+// techlab.q-co.jp/articles/134 사이트에서.., 압도적 감사.. ㅠㅠ
+export const VideoJsPlayer: FC<Props> = ({ sources, options: opt, onReady }) => {
+    const videoRef = useRef<HTMLVideoElement | Element | null>(null)
+    const playerRef = useRef<Player | null>(null)
+    const { handlePlay, isPlay, setIsPlay } = usePlay(playerRef)
+    const { isFullscreen, setIsFullscreen, handleFullscreen } = useFullscreen(playerRef)
+    const [videoReady, setVideoReady] = useState(false)
 
-export const VideoJsPlayer: FC<Props> = ({ sources, options, onReady }) => {
-    const videoRef = useRef<HTMLVideoElement>(null)
     useEffect(() => {
-        const video = videoRef.current
-        if (!video) return
-
-        const videojsOptions = { sources, ...options }
-        const player = videojs(video, videojsOptions)
-        player.on('ready', () => onReady?.(player))
-
-        return () => {
-            if (player) player.dispose()
+        if (!playerRef.current) {
+            const player = videojs(videoRef.current as Element, { sources, ...opt })
+            player.on('ready', () => {
+                onReady?.(player)
+                setVideoReady(true)
+                playerRef.current = player
+                playerRef.current.addClass('p-0')
+            })
+        } else {
+            const player = playerRef.current
+            player.src(sources)
         }
-    }, [onReady, options, sources])
+        return () => {
+            if (playerRef.current && !playerRef.current.isDisposed()) {
+                playerRef.current.dispose()
+                playerRef.current = null
+            }
+        }
+    }, [onReady, opt, sources, videoRef])
 
+    playerRef.current?.on('click', () => {
+        if (isPlay) {
+            setIsPlay(false)
+            playerRef.current?.pause()
+        } else {
+            setIsPlay(true)
+            playerRef.current?.play()
+        }
+    })
     return (
-        <div data-vjs-player>
-            {/* "video-js"는 video-js의 스타일 적용을 위해 필요 */}
-            <video ref={videoRef} className='video-js w-full' />
-        </div>
+        <>
+            <div className='relative h-fit'>
+                <video
+                    onContextMenu={(e) => e.preventDefault()}
+                    data-vjs-player
+                    ref={videoRef as MutableRefObject<HTMLVideoElement>}
+                    className='w-full h-fit'
+                />
+                {videoReady && !isPlay && (
+                    <section className='w-full h-full absolute inset-0 flex items-center justify-center'>
+                        <Badge onClick={handlePlay} className='w-16 h-10 inline-flex justify-center items-center' variant={'secondary'}>
+                            Play Cover
+                        </Badge>
+                    </section>
+                )}
+            </div>
+            <section className='flex'>
+                <Button variant={'outline'} onClick={handleFullscreen}>
+                    Fullscreen
+                </Button>
+                <Button variant={'outline'} onClick={handlePlay}>
+                    {isPlay ? 'Pause' : 'Play'}
+                </Button>
+            </section>
+        </>
     )
 }
-// techlab.q-co.jp/articles/134 사이트에서.., 압도적 감사.. ㅠㅠ
