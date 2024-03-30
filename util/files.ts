32c24eb0d9b22c3c820f305fa38d948b8db381ba
Hyunseok Byun <gumyoincirno@gmail.com>
2024-03-30 23:52:39 +0900

    Fix file util to reading with querystring

--- util/files.ts
+++ util/files.ts
@@ -1,15 +1,15 @@
 import { VideoExtensionName } from '@/constant/videoExtensionList'
-import { Folder, Video } from 'lucide-react'
 
+import { randomUUID } from 'crypto'
 import fs from 'fs'
 import path from 'path'
-import { randomUUID } from 'crypto'
 
-export const topPath = path.join(process.cwd(), 'public', 'video')
-
-export const getFolder = async () => {
-    const elements = fs.readdirSync(topPath) as string[]
-    const folder = elements.filter((ele) => fs.statSync(path.join(topPath, ele)).isDirectory()).map((ele) => ({ type: 'folder', name: ele }))
+export const topPath = path.join(process.cwd(), 'public')
+export const getFolder = async (folderPath?: string) => {
+    const elements = fs.readdirSync(path.join(topPath, folderPath || '')) as string[]
+    const folder = elements
+        .filter((ele) => fs.statSync(path.join(path.join(topPath, folderPath || ''), ele)).isDirectory())
+        .map((ele) => ({ type: 'folder', name: ele }))
     const files = elements
         .filter((ele) => VideoExtensionName.includes(ele.split('.').pop() || 'NONE'))
         .map((ele) => ({ type: 'file', name: `${ele}/`, tempId: randomUUID() }))
