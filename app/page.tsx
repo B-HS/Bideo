1a149713b3616df776443f8bc7ba5f167c5b1934
Hyunseok Byun <gumyoincirno@gmail.com>
2024-03-30 23:58:58 +0900

    Fix main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,14 @@
-import { Folder, Video } from 'lucide-react'
-import { getFolder, topPath } from '@/util/files'
-import { VideoJsPlayer } from '@/components/vjs-component'
+import FolderFileList from '@/components/folder-file-list'
+import PathManager from '@/components/path-manager'
 
-const Home = async () => {
-    const data = await getFolder()
+const Home = async ({ searchParams }: { searchParams?: { [key: string]: string | undefined } }) => {
+    const folrderPath = searchParams?.path || ''
+
     return (
-        <div className='p-10'>
-            {data.map((ele, idx) => (
-                <section key={idx} className='flex gap-2'>
-                    {ele.type === 'file' ? <Video /> : <Folder />}
-                    <span>{ele.name}</span>
-                </section>
-            ))}
-            <VideoJsPlayer
-                options={{ controls: false, fluid: true }}
-                sources={'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4'}
-            />
-            {data
-                .filter((ele) => ele.type === 'file')
-                .map((ele, idx) => (
-                    <section key={idx} className='w-full'>
-                        <p>{topPath + ele.name}</p>
-                        <VideoJsPlayer options={{ controls: false, fluid: true }} sources={`/video/${ele.name}`} />
-                    </section>
-                ))}
-        </div>
+        <section className='p-10 flex flex-col gap-3 border-b'>
+            <PathManager currentPath={folrderPath} />
+            <FolderFileList folderPath={folrderPath} />
+        </section>
     )
 }
 
